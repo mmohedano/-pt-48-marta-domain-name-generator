287ffa2dce2f76e560b67c38448d2bb76a8abcdb
Marta Mohedano <139046036+mmohedano@users.noreply.github.com>
2023-10-15 19:03:08 +0000

    end added

--- src/app.js
+++ src/app.js
@@ -13,11 +13,14 @@ let generateDomainName = () => {
   let pronoun = ["the", "our"];
   let adj = ["great", "big"];
   let noun = ["jogger", "racoon"];
+  let end = [".com", ".net"];
 
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
-      for (let k = 0; k < adj.length; k++)
-        console.log(pronoun[i], adj[j], noun[k] + ".com");
+      for (let k = 0; k < adj.length; k++) {
+        for (let l = 0; l < end.length; l++)
+          console.log(pronoun[i], adj[j], noun[k], end[l]);
+      }
     }
   }
 };
