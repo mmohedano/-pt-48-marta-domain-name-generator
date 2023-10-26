5376bf1976eebc54eecd1e6cf4d7b336178a4dc7
Marta Mohedano <139046036+mmohedano@users.noreply.github.com>
2023-10-26 12:00:42 +0000

    forEach

--- src/app.js
+++ src/app.js
@@ -6,21 +6,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = () => {
-  document.querySelector("#domainNames").innerHTML = generateDomainName();
-};
-let generateDomainName = () => {
-  let pronoun = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["jogger", "racoon"];
-  let end = [".com", ".net"];
+const pronoun = ["the", "our"];
+const adj = ["great", "big"];
+const noun = ["jogger", "racoon"];
+const end = [".com", ".net"];
 
-  for (let i = 0; i < pronoun.length; i++) {
-    for (let j = 0; j < adj.length; j++) {
-      for (let k = 0; k < adj.length; k++) {
-        for (let l = 0; l < end.length; l++)
-          console.log(pronoun[i], adj[j], noun[k], end[l]);
-      }
-    }
-  }
+document.generateDomainName = () => {
+  const newState = process();
+  render(newState);
 };
+function process() {
+  let state = [];
+  pronoun.forEach(i => {
+    adj.forEach(j => {
+      noun.forEach(k => {
+        end.forEach(l => {
+          state = [...state, `${i}${j}${k}${l}`];
+        });
+      });
+    });
+  });
+  return state;
+}
+function render(stat) {
+  const list = stat.map(name => `<li>${name}</li>`).join("");
+  const unorderedList = document.querySelector(".listDomains");
+  unorderedList.innerHTML = list;
+}
